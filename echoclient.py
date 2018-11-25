fb28b0f1c7ef0e376dd8bc89178c97355b225dc7
11mink <11mink@naver.com>
2018-11-25 18:22:38 +0900

    First Commit

---
+++ echoclient.py
@@ -0,0 +1,45 @@
+import sys
+import select
+from socket import *
+
+def usage():
+	if len(sys.argv) != 3:
+		print("syntax : echoclient <host> <port>")
+		print("sample : echoclient 127.0.0.1 1234")
+		sys.exit()
+
+def main():
+	usage()
+
+	ip = sys.argv[1]
+	port = sys.argv[2]
+	addr = (ip, int(port))
+
+	client = socket(AF_INET, SOCK_STREAM)
+	client.connect(addr)
+	print("connected")
+
+	inputs = [sys.stdin, client]
+	size = 1024
+	received = True
+	timeout = 0.5
+
+	while True:
+		readable, writable, exceptional = select.select(inputs,[],[],timeout)
+
+		if readable == [] and received == False:
+			print("disconnected")
+			client.close()
+			sys.exit()
+
+		for sock in readable:
+			if sock == client:
+				print("<Server> " + client.recv(size))
+				received = True
+
+			elif sock == sys.stdin:
+				client.send(raw_input())
+				received = False
+
+if __name__ == "__main__":
+	main()
---
+++ echoserver.py
@@ -0,0 +1,52 @@
+import sys
+import threading
+import select
+from socket import *
+
+def usage():
+	print("syntax : echoserver <port> [-b]")
+	print("sample : echoserver 1234 -b")
+
+def main():
+	broadcast = False
+	if len(sys.argv) == 3 and sys.argv[2] == "-b":
+		broadcast = True
+	elif len(sys.argv) != 2:
+		usage()
+		sys.exit()
+
+	ip = "127.0.0.1"
+	port = sys.argv[1]
+	addr = (ip, int(port))
+
+	serverSocket = socket(AF_INET, SOCK_STREAM)
+	serverSocket.bind(addr)
+	serverSocket.listen(5)
+	socketList = [serverSocket]
+	size = 1024
+
+	while True:
+		readable, writable, exceptional = select.select(socketList, [], [], 10)
+
+		for sock in readable:
+			if sock == serverSocket:
+				clientSocket, addr = serverSocket.accept()
+				socketList.append(clientSocket)
+				print("client connected", addr)
+
+			else:
+				data = sock.recv(size)
+				if not data:
+					sock.close()
+					socketList.remove(sock)
+				elif broadcast:
+					for client in socketList:
+						if client == serverSocket:
+							continue
+						else:
+							client.send(data)
+				else:
+					clientSocket.send(data)
+
+if __name__ == "__main__":
+	main()
